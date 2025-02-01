e7e697637d12d19f7b010210eb98da18d4426a47
Theo Akande <ta722@ic.ac.uk>
2025-02-01 13:01:36 +0000

    feat: initial webscraper

--- src/webscraper.py
+++ src/webscraper.py
@@ -1,1 +1,15 @@
 # Scrape news articles from the web
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_news_articles(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    articles = soup.find_all('article')
+    output = []
+    for article in articles:
+        title = article.h2.a.text
+        body = article.find('div', class_='entry-content').p.text
+        output.append({'title': title, 'body': body})
+    return output
