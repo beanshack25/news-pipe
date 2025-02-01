cbd757e75063b4147e9e7ba58c3b9def4e83272b
Theo Akande <ta722@ic.ac.uk>
2025-02-01 12:55:03 +0000

    feat: article pipe class

--- src/article.py
+++ src/article.py
@@ -1,1 +1,14 @@
-# article class
+# Article class
+
+# Stores list of topics, text content, sentiment, timeline, and other metadata of an article
+
+class Article:
+
+    def __init__(self, text, topics, sentiment, timeline, metadata):
+        self.text = text
+        self.topics = topics
+        self.sentiment = sentiment
+        self.timeline = timeline
+        self.metadata = metadata
+    
+    
