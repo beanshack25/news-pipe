540148f341dc6e356d903185e9e39345f9190d81
Theo Akande <ta722@ic.ac.uk>
2025-02-01 14:57:15 +0000

    fix: query in article

--- src/article.py
+++ src/article.py
@@ -8,10 +8,11 @@ from sentiment import Sentiment
 
 class Article:
 
-    def __init__(self, text: str, topics: list[str], sentiment: Sentiment, timestamp: datetime.date, link: str):
+    def __init__(self, text: str, topics: list[str], sentiment: Sentiment, timestamp: datetime.date, link: str, query: str):
         self.text = text
         self.topics = topics
         self.sentiment = sentiment
         self.timestamp = timestamp
         self.link = link
+        self.query = query
     
