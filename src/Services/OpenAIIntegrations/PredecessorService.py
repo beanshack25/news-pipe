bf3ec86f1f7dbb45cfe6780edee08bdd01d74ce1
rahul <user.name>
2025-02-01 18:02:40 +0000

    feat: stuff

--- src/Services/OpenAIIntegrations/PredecessorService.py
+++ src/Services/OpenAIIntegrations/PredecessorService.py
@@ -15,7 +15,9 @@ def main():
     newArti = ArticleNode("Putin declares war on Ukraine",
                       'Since the invasion began on 24 February 2022, the Russian government and media outlets have consistently used the term “special military operation” to refer to the attacks on its neighbour’s territory. Russia made extensive efforts to refrain from labelling the military conflict in Ukraine a war.',
                       "2025-02-01T12:28:31Z",
-                      "stub")
+                      "stub",
+                          0,
+                          10)
     print(query(newArti))
 
 if __name__ == '__main__':
