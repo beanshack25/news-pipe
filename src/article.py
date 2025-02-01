2681fb2af610f1b725bf93d285f2d7d713ddc528
Theo Akande <ta722@ic.ac.uk>
2025-02-01 16:01:53 +0000

    feat: newsapi possible

--- src/article.py
+++ src/article.py
@@ -8,11 +8,20 @@ from sentiment import Sentiment
 
 class Article:
 
-    def __init__(self, text: str, topics: list[str], sentiment: Sentiment, timestamp: datetime.date, link: str, query: str):
+    def __init__(self, text: str, topics: list[str], sentiment: Sentiment, timestamp: datetime.date, link: str, headline: str):
         self.text = text
         self.topics = topics
         self.sentiment = sentiment
         self.timestamp = timestamp
         self.link = link
-        self.query = query
+        self.headline = headline
+
+    def __repr__(self):
+        string = ""
+        string += f"Headline: {self.headline}\n"
+        string += f"Published: {self.timestamp}\n"
+        string += f"Link: {self.link}\n"
+        string += f"Text: \n{self.text}\n"
+
+        return string
     
